ed587822b1749e2ed2bd164518bb7c344fc19d69
Shorya Hayaran <Shoryahayaran@gmail.com>
2021-12-13 11:37:47 +0530

    NLTK

--- Using_NLTK.py
+++ Using_NLTK.py
@@ -1,9 +1,9 @@
+#this simple program shows the use of python and nltk to tokenize sentences and words
 from nltk.tokenize import sent_tokenize, word_tokenize
-example_text='"I am not a psychopath, Anderson. I am a high functioning sociopath, do you research": Sherlock Holmes'
+example_text='"I am not a psychopath, Anderson. I am a high functioning sociopath, do you research": Mr. Sherlock Holmes'
 
 print(sent_tokenize(example_text))
 print(word_tokenize(example_text))
-#alt+3 comment out a block
 example= "There is nothing more deceptive than obvious fact."
 print(sent_tokenize(example))
 print(word_tokenize(example))
